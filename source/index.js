1a5e6f48cabea5cba199cfd2f2b8d14ca9a4ebc1
Aksinister <91866119+Aksinister@users.noreply.github.com>
2025-10-24 13:06:15 +1000

    Added API functionality to JS
    Added API functionality to my JS so that the SPA displays real time temperature data from any city in the world.

--- source/index.js
+++ source/index.js
@@ -1,9 +1,25 @@
 function manageSearchRequest(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input-bar");
+
+  searchCity(searchInput.value);
+}
+
+function searchCity(city) {
+  let apiKey = "7300c6775obt0415fe6635cd0da0d3fe";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(updateWeatherStats);
+}
+
+function updateWeatherStats(response) {
+  let temperatureItem = document.querySelector("#temperature-item");
+  let temp = response.data.temperature.current;
   let cityItem = document.querySelector("#city-item");
-  cityItem.innerHTML = searchInput.value;
+  temperatureItem.innerHTML = Math.round(temp);
+  cityItem.innerHTML = response.data.city;
 }
 
 let searchFormItem = document.querySelector("#search-form");
 searchFormItem = addEventListener("submit", manageSearchRequest);
+
+searchCity("Brisbane");
